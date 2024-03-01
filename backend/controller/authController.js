fd068b381e6e3363c6a2e2b734c4b590c75149c2
Den Kong <denusklo@gmail.com>
2024-03-01 18:43:12 +0800

    add controller and generate token file

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generate.token.js";
 
 export const signup = async (req, res) => {
     try {
@@ -39,6 +40,7 @@ export const signup = async (req, res) => {
         });
 
         if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
             await newUser.save();
     
             res.status(201).json({
@@ -56,11 +58,39 @@ export const signup = async (req, res) => {
         res.status(500).json({error: "Internal Server Error"});
     }
 };
-export const login = (req, res) => {
-    res.send("login");
-    console.log("login");
+
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 };
+
 export const logout = (req, res) => {
-    res.send("logout");
-    console.log("logout");
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({
+            message: "Logout sucessfully",
+        });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 };
