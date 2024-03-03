2e2ea4569cfbefecde3e7587d638707080f1f4c7
Den Kong <denusklo@gmail.com>
2024-03-03 14:50:22 +0800

    sign up function on frontend

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GenderCheckBox from './GenderCheckBox';
+import { Link } from 'react-router-dom';
+import useSignup from '../../hooks/useSignup';
 
 const SignUp = () => {
+    const [inputs, setInputs] = useState({
+        fullName: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+        gender: '',
+    });
+
+    const {loading, signup} = useSignup();
+
+    const handleCheckboxChange = (gender) => {
+        setInputs({...inputs, gender})
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await signup(inputs);
+    };
+
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
             <h1 className="text-3xl font-semibold text-center text-gray-300">
                 Sign Up <span className='text-blue-500'>ChatApp</span>
             </h1>
-            <form action="#">
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="" className='label p-2'>
                         <span className="text-base label-text">Full Name</span>
                     </label>
-                    <input type="text" placeholder='John Doe' className='w-full input input-bordered h-10' />
+                    <input type="text" placeholder='John Doe' className='w-full input input-bordered h-10'
+                    value={inputs.fullName}
+                    onChange={(e) => setInputs({...inputs, fullName: e.target.value})}/>
                 </div>
                 <div>
                     <label htmlFor="" className='label p-2'>
@@ -21,7 +44,20 @@ const SignUp = () => {
                             Username
                         </span>
                     </label>
-                    <input type="text" placeholder='johndoe' className='w-full input input-bordered h-10' />
+                    <input type="text" placeholder='johndoe' className='w-full input input-bordered h-10' 
+                    value={inputs.username}
+                    onChange={(e) => setInputs({...inputs, username: e.target.value})}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="" className='label p-2'>
+                        <span className='text-base label-text'>
+                            Password
+                        </span>
+                    </label>
+                    <input type="password" placeholder='EnterPassword' className='w-full input input-bordered h-10'
+                    value={inputs.password}
+                    onChange={(e) => setInputs({...inputs, password: e.target.value})}/>
                 </div>
                 <div>
                     <label htmlFor="" className='label p-2'>
@@ -29,16 +65,21 @@ const SignUp = () => {
                             Confirm Password
                         </span>
                     </label>
-                    <input type="password" placeholder='Confirm Password' className='w-full input input-bordered h-10' />
+                    <input type="password" placeholder='Confirm Password' className='w-full input input-bordered h-10'
+                    value={inputs.confirmPassword}
+                    onChange={(e) => setInputs({...inputs, confirmPassword: e.target.value})}/>
                 </div>
 
-                <GenderCheckBox/>
+                <GenderCheckBox onCheckboxChange={handleCheckboxChange} selectedGender={inputs.gender}/>
 
-                <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                <Link to="/login" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
                     Already have an account?
-                </a>
+                </Link>
                 <div>
-                    <button className='btn btn-block btn-sm mt-2 border border-slate-600'>Sign Up</button>
+                    <button className='btn btn-block btn-sm mt-2 border border-slate-600'
+                    disabled={loading}>
+                        { loading ? <span className='loading loading-spinner'></span> : "Sign Up" }
+                    </button>
                 </div>
             </form>
         </div>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 1131,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:1130"
+      }
+    }
+  }
 })
